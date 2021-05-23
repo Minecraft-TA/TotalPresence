f7c20cef5912fa5e2f3ab9383799c575a569c330
tth05 <36999320+tth05@users.noreply.github.com>
2021-05-23 19:17:37 +0200

    Fixed modid to follow naming conventions

--- src/main/java/com/github/minecraft_ta/totalpresence/TotalPresence.java
+++ src/main/java/com/github/minecraft_ta/totalpresence/TotalPresence.java
@@ -20,7 +20,7 @@ import java.io.File;
 )
 public class TotalPresence {
 
-    public static final String MOD_ID = "totalpresence";
+    public static final String MOD_ID = "total_presence";
     public static final String MOD_NAME = "TotalPresence";
     public static final String VERSION = "1.0.0";
 
